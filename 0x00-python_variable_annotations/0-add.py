e9162093dfc01931df9b38966f9fbd3d87577695
KipEric <kipkoecheric97@gmail.com>
2023-07-08 11:50:58 +0300

    correc

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -4,4 +4,5 @@ A type-annotated function add that takes a float a and a float b
 as arguments and returns their sum as a float.
 """
 def add(a: float, b: float) -> float:
+    """ A simple function that return sum of two numbers a and b"""
     return a + b
