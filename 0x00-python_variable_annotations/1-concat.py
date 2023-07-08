f8930d53255e71caf6d974dc1cc910166cae5324
KipEric <kipkoecheric97@gmail.com>
2023-07-08 11:54:11 +0300

    correc1

--- 0x00-python_variable_annotations/1-concat.py
+++ 0x00-python_variable_annotations/1-concat.py
@@ -3,6 +3,8 @@
 A type-annotated function concat that takes a string str1
 and a string str2 as arguments and returns a concatenated string.
 """
+
+
 def concat(str1: str, str2: str) -> str:
     """ Basic function that concatinate two strings"""
     return str1 + str2
